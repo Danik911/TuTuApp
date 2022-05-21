16e9be660501777a6433f57817580300c459936d
Danik <porvipop@mail.ru>
2022-05-21 16:46:59 +0300

    Inject TrainDatabase

---
+++ app/src/main/java/com/danik/tutuapp/di/DatabaseModule.kt
@@ -0,0 +1,28 @@
+package com.danik.tutuapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.danik.tutuapp.data.local.TrainDatabase
+import com.danik.tutuapp.util.Constants.TRAIN_DATABASE
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): TrainDatabase {
+        return Room.databaseBuilder(
+            context,
+            TrainDatabase::class.java,
+            TRAIN_DATABASE
+        ).build()
+    }
+
+}
